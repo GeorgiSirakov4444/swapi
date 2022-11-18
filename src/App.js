3224ac48b70d0256c61b2dabdbefca93458b0b29
Georgi Sirakov <georgi.sirakov74@gmail.com>
2022-11-18 16:22:54 +0200

    handling Error

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React, {useState} from 'react';
-import MoviesList from './components/MoviesList';
-import './App.css';
+import React, { useState } from "react";
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
-  async function fetchMoviehandler () {
+  async function fetchMoviehandler() {
     setIsLoading(true);
-    const response = await fetch('https://swapi.dev/api/films/');
-    const data = await response.json();
+    setError(null);
 
-    const transformedMoviesObjects = data.results.map((transformedData) => {
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+      
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+      const data = await response.json();
+
+      const transformedMoviesObjects = data.results.map((transformedData) => {
         return {
           id: transformedData.episode_id,
           title: transformedData.title,
           openingText: transformedData.opening_crawl,
           releaseDate: transformedData.release_date,
         };
-    });
-    setMovies(transformedMoviesObjects);
+      });
+      setMovies(transformedMoviesObjects);
+    } catch (error) {
+      setError(error.message);
+    }
     setIsLoading(false);
+  }
+
+  let content = <p>Found no movies :(</p>;
+
+  if(movies.length > 0){
+    content = <MoviesList movies={movies} />;
+  }
+  if(error){
+    content = <p>{error}</p>;
+  }
+  if(isLoading){
+    content = <p>Loading ...</p>;
   }
 
   return (
@@ -29,9 +52,7 @@ function App() {
         <button onClick={fetchMoviehandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found no movies :(</p>}
-        {isLoading && <p>Loading ...</p>}
+        {content}
       </section>
     </React.Fragment>
   );
---
+++ src/components/Errors.js
@@ -0,0 +1,8 @@
+const https = ('HTTP response status. Responses are grouped in five classes:')
+const httpsSTATUS = {
+    Informational: '100-199',
+    Successful: '200-299',
+    Redirection: '300-399',
+    ClientError: '400-499',
+    ServerError: '500-599'
+};
